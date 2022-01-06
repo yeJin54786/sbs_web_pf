2ff13c602d8e3faed36b5697af2974b73f4727e1
yejin54786 <qwer6894@naver.com>
2022-01-06 21:06:03 +0900

    last

---
+++ 디자인기법/svg만들기/js/script.js
@@ -0,0 +1,9 @@
+
+$(function() {
+    $('.svg').scrolla({
+        // default
+        mobile: false, // disable animation on mobiles
+        once: false, // only once animation play on scroll
+        animateCssVersion: 4 // used animate.css version (3 or 4)
+    });
+})

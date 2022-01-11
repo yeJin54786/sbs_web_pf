a6b173e6f9170e2b11660ef24b0835960391e852
yejin54786 <qwer6894@naver.com>
2022-01-11 15:42:40 +0900

    pf

---
+++ lyj_pf_ivo/js/script.js
@@ -0,0 +1,22 @@
+$(function () {
+
+    // 스와이퍼 시작!
+    const swiper = new Swiper('.main-swiper', {
+        
+        direction: 'horizontal',        // 슬라이드 방향 : 'vertical', 'horizontal'
+        loop: true,                     // 반복여부
+        autoplay: {                     // 자동재생    
+            delay: 3000,                // 슬라이드 당 지연시간 (ms) 
+            pauseOnMouseEnter: true,    // 슬라이드에 마우스 올려두면, 자동재생 멈춤
+            disableOnInteraction: true, // 인터렉션(화살표,드래그,...) 중의 자동재생 비활성화여부
+        },
+
+
+        slidesPerView: 1,               // 보여지는 슬라이드 개수
+        spaceBetween: 0,               // 슬라이드 간 여백
+        
+
+        
+    });
+
+})

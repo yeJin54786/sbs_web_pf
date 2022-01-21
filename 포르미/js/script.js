ef5788b5d9c9dbb1344fcdbf024699eec5a5a26c
yejin54786 <qwer6894@naver.com>
2022-01-21 11:43:27 +0900

    220120

--- 포르미/js/script.js
+++ 포르미/js/script.js
@@ -1,11 +1,9 @@
 $(function() {
 
-
-    // 슬릭 슬라이드 시작!
+    // 메인 슬라이드
     $('.slide-container').slick({
         dots: true,
-        arrows: true,
-
+        arrows: false,
 
         autoplay: true,                 // 자동재생 여부
         autoplaySpeed: 3000,            // 자동재생 슬라이드 시간
@@ -22,31 +20,17 @@ $(function() {
         pauseOnDotsHover: true,         // 페이지네이션 호버 시 자동재생 멈춤
         pauseOnFocus: true,             // 포커스 시, 자동재생 멈춤
         pauseOnHover: true,             // 호버 시, 자동재생 멈춤
-
-
-        /* 효과 */
-        // fade: true,
-        // cssEase: 'linear',
-
-
-        // 네비게이션 화살표 커스텀
-        //prevArrow: '<button class="uslide-arrow prev-arrow"></button>',
-        //nextArrow: '<button class="slide-arrow next-arrow"></btton>',
-
-
     })
 
-
-    // 인기상품 슬라이드
+    // 인기 상품 슬라이드
     $('.product-slide').slick({
         dots: false,
         arrows: true,
 
-
         autoplay: true,                 // 자동재생 여부
         autoplaySpeed: 3000,            // 자동재생 슬라이드 시간
         infinite: true,                 // 무한 반복 
-        speed:500,                      // 슬라이드가 전환되는 시간
+        speed:800,                      // 슬라이드가 전환되는 시간
 
 
         // centerMode: true,            // 센터 모드
@@ -58,23 +42,35 @@ $(function() {
         pauseOnDotsHover: true,         // 페이지네이션 호버 시 자동재생 멈춤
         pauseOnFocus: true,             // 포커스 시, 자동재생 멈춤
         pauseOnHover: true,             // 호버 시, 자동재생 멈춤
-
-
-        /* 효과 */
-        // fade: true,
-        // cssEase: 'linear',
-
-
-        // 네비게이션 화살표 커스텀
-        //prevArrow: '<button class="uslide-arrow prev-arrow"></button>',
-        //nextArrow: '<button class="slide-arrow next-arrow"></btton>',
-
-
     })
 
-    // 슬릭 슬라이드 - 화살표 벗어날 때  
-    $('.slick-prev, slick-next').on('mouseout', function() {
-        $('.slick-prev').css({'background-image':'url(../img/arrow_s.png) no-repeat center center'})
-    })
+    // 3초마다 멀티 슬라이드 타이머 적용
+    let slide = setInterval(multSlide, 3000)
+
 })
+
+// 슬라이드
+let index = 0
+function multSlide() {
+    index++
+
+
+    //상품 제목
+    $('.left-box .title ul li').removeClass('active')
+    $('.left-box .title ul li:nth-child(' + index + ')').addClass('active')
+
+    //상품 설명
+    $('.left-box .info ul li').removeClass('active')
+    $('.left-box .info ul li:nth-child(' + index + ')').addClass('active')
+
+    //상품 이미지
+    $('.left-box .img ul li').removeClass('active')
+    $('.left-box .img ul li:nth-child(' + index + ')').addClass('active')
+
+
+
+
+    if( index == 4 )
+        index = 0
+}
 
